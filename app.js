1885a0e431ae7ef888c901bbbd9ac5d277b435ab
victor xavier <victortpxavier@gmail.com>
2018-07-27 18:24:02 +0100

    commit 7

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 "use strict";
 const express = require("express");
-const requestHandlers = require("./scripts/request-handlers.js");
+//const requestHandlers = require("./scripts/request-handlers.js");
 const bodyParser = require("body-parser");
 const app = express();
 const port = process.env.PORT || 8081;
@@ -10,6 +10,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("www"));
 
 //roteamento
+/**
 app.get("/user", requestHandlers.getUsers);
 app.get("/questions", requestHandlers.getQuestions);
 app.get("/questions/:id/answers", requestHandlers.getAnswer);
@@ -22,7 +23,7 @@ app.delete("/question/:id", requestHandlers.removeQuestion);
 
 app.post("/question/:id/answers", requestHandlers.createAnswer);
 app.delete("/question/:id/answers/:id_a", requestHandlers.deteleAnswer);
-
+*/
 app.listen(port, function () {
     console.log("Server running at http://localhost:" + port);
 });
