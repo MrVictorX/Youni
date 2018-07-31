e3c56b319030bb8b39267c39bc678ed8ab466ad2
victor xavier <victortpxavier@gmail.com>
2018-07-31 11:13:55 +0100

    commit 12

--- www/scripts/scripts.js
+++ www/scripts/scripts.js
@@ -10,9 +10,9 @@ window.onload = function (event) {
 
 function Information(id) {
     this.id = id;
-    this.users = [];
-    this.questions = [];
-    this.answers = [];
+    this.users = createTestUsers();
+    this.questions = createTestQuestions();
+    this.answers = createTestAnswers();
 };
 
 function User(id, name, email) {
@@ -21,25 +21,46 @@ function User(id, name, email) {
     this.email = email;
 };
 
-function Question(id, content, document, anounymous) {
+function Question(id, content, document, anounymous, user) {
     this.id = id;
     this.content = content;
     this.document = document;
     this.votes = 0;
     this.anounymous = anounymous;
+    this.user = user;
 };
 
-function Answer(id, content, document) {
+function Answer(id, content, document, question, user) {
     this.id = id;
     this.content = content;
     this.document = document;
     this.votes = 0;
+    this.question = question;
+    this.user = user;
 };
 
+Information.prototype.createTestUsers = function () {
+    for (var i = 0; i < 10; i++) {
+        this.users.push(new User(i, "User" + i + 1, "User" + i + 1 + "@gmail.com"));
+    }
+}
+
+Information.prototype.createTestQuestions = function () {
+    for (var i = 0; i < 5; i++) {
+        this.questions.push(new Question(i, "bapada boopy" + i + 1 + "?", null, 0, false, this.users[i]));
+    }
+}
+
+Information.prototype.createTestAnswers = function () {
+    var userCounter = 10;
+    for (var i = 0; i < 5; i++) {
+        this.answers.push(new Answer(i, "boopidy bappy"+i+1, null,0, this.questions[i], this.users[userCounter--]));
+    }
+}
 
 /* User*/
 
-
+/**
 Information.prototype.showUser = function () {
     var table = document.getElementById("tableUser");
     while (table.firstChild) {
@@ -49,7 +70,8 @@ Information.prototype.showUser = function () {
         table.appendChild(inTableUser(this.users[i]));
     }
 };
-
+ */
+/**
 Information.prototype.getUsers = function () {
     var xhr = new XMLHttpRequest();
     xhr.open("GET", "http://localhost:8081/user", true);
@@ -65,6 +87,7 @@ Information.prototype.getUsers = function () {
     };
     xhr.send();
 };
+ */
 
 /* Question */
 
